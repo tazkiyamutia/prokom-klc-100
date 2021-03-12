449b54e9b21d90d7d16c2aedfbaf3038640d44c2
Tazkiya Mutia Yogasani <80145112+tazkiyamutia@users.noreply.github.com>
2021-03-12 11:16:11 +0700

    Add tugas 1

---
+++ Tugas 1 No 1.py
@@ -0,0 +1,16 @@
+# Praktikum Programa Komputer
+
+print("Tazkiya Mutia Yogasani")
+print("I0320100")
+print("Saya tinggal di Kampung Jetak, Desa Bolon, Colomadu, Karanganyar."
+      " Rumah yang berada di sekitar rumah saya, adalah rumah "
+      "saudara-saudara saya. Sebelah Selatan rumah saya adalah jalan kampung. "
+      "Terdapat satu masjid di kampung saya. Letaknya tepat di samping "
+      "gedung IPHI dan terdapat empat toko kelontong yang berada di kampung saya.")
+
+
+# Hitung Umur
+
+umur_dalam_bulan = 20 * 12
+bulan_lebih = 6
+print("Umur saya " + str(umur_dalam_bulan + bulan_lebih) + " Bulan")
